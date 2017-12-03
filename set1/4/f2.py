d0ae4dbdeee800b7e85b164faeb82fafe140acb4
Aditya Geria <adgeria@gmail.com>
2017-12-03 00:51:15 -0500

    still trying

--- set1/4/f2.py
+++ set1/4/f2.py
@@ -6,12 +6,12 @@ fitness = word_score()
 decrypted = ""
 decrypted_strings = []
 fitness_dict = {}
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`!@#$%^&*()_+=-|\}]{[]};:<,>.?/"
+alphabet = "\n\t\v\r\'\"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~\\`!@#$%^&*()_+=-|\}]{[]};:<,>.?/"
 
 for ctext in fo:
     #ctext = fo.readline()
     #print ctext
-    ctext = ctext.strip()#.decode("hex")
+    ctext = ctext.strip().decode("hex")
     for a in alphabet:
         for c in ctext:
             try:
@@ -26,7 +26,7 @@ for ctext in fo:
 for i in range(0, len(decrypted_strings)):
     x, y = fitness.score(decrypted_strings[i])
     #print i
-    if x > -93.0:
+    if x > -51.0:
         print " " + str(x) + "  " + decrypted_strings[i]
     fitness_dict[decrypted_strings[i]] = x
 
